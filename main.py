eacf33c7bcdb287a2340fa0a1e74202d9abb6f64
ernis <ernis.meshi@student.uni.pr-edu>
2022-02-09 10:43:55 +0100

    We changed hi to hello and added new lines

--- main.py
+++ main.py
@@ -7,11 +7,14 @@
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Hello, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    print('12nd,sdf')
+    print('Where do you live')
     print('12nd')
+
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
