c6ba48cc19134cbf5ca998ecead445bb5315c3e4
ernis <ernis.meshi@student.uni.pr-edu>
2022-02-09 11:03:19 +0100

    testing new function

--- main.py
+++ main.py
@@ -8,13 +8,14 @@
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hello, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
+def my_function(a,b):
+    return a+b
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
     print('12nd,sdf')
     print('Where do you live')
     print('12nd')
+    print(my_function(2,4))
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
